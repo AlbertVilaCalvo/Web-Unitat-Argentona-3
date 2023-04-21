d92739b5c845d4717426cc04a1cbbd1d4ca0f67d
Albert Vila Calvo <4823860+AlbertVilaCalvo@users.noreply.github.com>
2023-04-21 18:04:18 +0200

    Noms candidats a /llista

---
+++ src/data/llista2023.ts
@@ -0,0 +1,78 @@
+interface Candidat {
+  nom: string
+}
+
+export const llista2023: Candidat[] = [
+  {
+    nom: 'Toni Segarra',
+  },
+  {
+    nom: 'Isabel Sunyer',
+  },
+  {
+    nom: 'Joan Boba',
+  },
+  {
+    nom: 'Isabel Lladó',
+  },
+  {
+    nom: 'Isidre Viñas',
+  },
+  {
+    nom: 'Rut Ligos',
+  },
+  {
+    nom: 'Eusebi Traby',
+  },
+  {
+    nom: 'Araceli Ferrer',
+  },
+  {
+    nom: 'Xavier Rodríguez',
+  },
+  {
+    nom: 'Imma Collet',
+  },
+  {
+    nom: 'Joan Vera',
+  },
+  {
+    nom: 'Elizabeth Guirado',
+  },
+  {
+    nom: 'Rosend',
+  },
+  {
+    nom: 'Monste Terricabras',
+  },
+  {
+    nom: 'Albert Vila',
+  },
+  {
+    nom: 'Isabel Cunyat',
+  },
+  {
+    nom: 'Savino Rico',
+  },
+  {
+    nom: 'Angelina Carbonell',
+  },
+  {
+    nom: 'Toni Martí',
+  },
+  {
+    nom: 'Carme Famadas',
+  },
+  {
+    nom: 'Ricard Aymerich',
+  },
+  {
+    nom: 'Margarida Colomer',
+  },
+  {
+    nom: 'Pep Famadas',
+  },
+  {
+    nom: 'Cristina Navarro',
+  },
+]
