00bf08636b591be43b24326c05407f4985194be0
Albert Vila Calvo <4823860+AlbertVilaCalvo@users.noreply.github.com>
2023-04-22 17:03:52 +0200

    Get rid of trailing slashes in Netlify
    Netlify behavior with trailing slashes is explained at
    https://answers.netlify.com/t/support-guide-how-can-i-alter-trailing-slash-behaviour-in-my-urls-will-enabling-pretty-urls-help/31191
    
    At the moment the URLs are /llista/ and /blog/ because we are generating
    the pages /llista/index.html and /blob/index.html. To get rid of the trailing
    slash we need to instead generate /llista.html and /blog.html with the
    format 'file'.
    
    build.format docs: https://docs.astro.build/en/reference/configuration-reference/#buildformat
    
    NavLink needs to be changed to continue being highlighted because pathname now is '/blog/2.html'.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  build: {
+    // We don't want trailing slashes (ie /llista/). Netlify adds a trailing
+    // slash if we generate /llista/index.html instead of /llista.html, see
+    // https://answers.netlify.com/t/support-guide-how-can-i-alter-trailing-slash-behaviour-in-my-urls-will-enabling-pretty-urls-help/31191
+    format: 'file',
+  },
+})
