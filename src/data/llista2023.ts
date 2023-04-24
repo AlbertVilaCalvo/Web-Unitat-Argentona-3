a96d3a50e7890d0140ff4fc17c07edbdbdd8ea06
Albert Vila Calvo <4823860+AlbertVilaCalvo@users.noreply.github.com>
2023-04-24 23:56:19 +0200

    Fotos llista

--- src/data/llista2023.ts
+++ src/data/llista2023.ts
@@ -1,59 +1,80 @@
+import type { CloudinaryImageId } from '../misc/cloudinary'
+
 interface Candidat {
   nom: string
+  image: CloudinaryImageId
 }
 
 export const llista2023: Candidat[] = [
   {
     nom: 'Toni Segarra',
+    image: '2023-llista-frontal/1_DSC02904',
   },
   {
     nom: 'Isabel Sunyer',
+    image: '2023-llista-frontal/2_DSC02769b1',
   },
   {
     nom: 'Joan Boba',
+    image: '2023-llista-frontal/3_DSC02803b_opt',
   },
   {
     nom: 'Isabel Lladó',
+    image: '2023-llista-frontal/4_DSC02941b_opt',
   },
   {
     nom: 'Isidre Viñas',
+    image: '2023-llista-frontal/5_DSC02818b_opt',
   },
   {
     nom: 'Rut Ligos',
+    image: '2023-llista-frontal/6_DSC03096',
   },
   {
     nom: 'Eusebi Traby',
+    image: '2023-llista-frontal/7_DSC02755_opt',
   },
   {
     nom: 'Araceli Ferrer',
+    image: '2023-llista-frontal/8_DSC02819',
   },
   {
     nom: 'Xavier Rodríguez',
+    image: '2023-llista-frontal/9_DSC03094',
   },
   {
     nom: 'Imma Collet',
+    image: '2023-llista-frontal/10_DSC03089',
   },
   {
     nom: 'Joan Vera',
+    image: '2023-llista-frontal/11_DSC02897',
   },
   {
     nom: 'Elizabeth Guirado',
+    image: '2023-llista-frontal/12_DSC03195',
   },
   {
     nom: 'Rosend',
+    image: '2023-llista-frontal/13_DSC03178',
   },
   {
     nom: 'Monste Terricabras',
+    image: '2023-llista-frontal/14_DSC03320_opt',
   },
   {
     nom: 'Albert Vila',
+    image: '2023-llista-frontal/15_DSC03079',
   },
   {
     nom: 'Isabel Cunyat',
+    image: '2023-llista-frontal/16_DSC03215',
   },
   {
     nom: 'Savino Rico',
+    image: '2023-llista-frontal/17_DSC03182',
   },
+  /*
   {
     nom: 'Angelina Carbonell',
   },
@@ -75,4 +96,5 @@ export const llista2023: Candidat[] = [
   {
     nom: 'Cristina Navarro',
   },
+  */
 ]
--- src/misc/cloudinary.ts
+++ src/misc/cloudinary.ts
@@ -2,4 +2,24 @@ import { Cloudinary } from '@cloudinary/url-gen'
 
 export const cloudinary = new Cloudinary({ cloud: { cloudName: 'albertvila' } })
 
-export type CloudinaryImageId = 'DSC02847c_copia'
+export type CloudinaryImageId =
+  // 2023-02-25-sessio-fotos-carrer
+  | 'DSC02847c_copia'
+  // 2023-llista-frontal
+  | '2023-llista-frontal/1_DSC02904'
+  | '2023-llista-frontal/2_DSC02769b1'
+  | '2023-llista-frontal/3_DSC02803b_opt'
+  | '2023-llista-frontal/4_DSC02941b_opt'
+  | '2023-llista-frontal/5_DSC02818b_opt'
+  | '2023-llista-frontal/6_DSC03096'
+  | '2023-llista-frontal/7_DSC02755_opt'
+  | '2023-llista-frontal/8_DSC02819'
+  | '2023-llista-frontal/9_DSC03094'
+  | '2023-llista-frontal/10_DSC03089'
+  | '2023-llista-frontal/11_DSC02897'
+  | '2023-llista-frontal/12_DSC03195'
+  | '2023-llista-frontal/13_DSC03178'
+  | '2023-llista-frontal/14_DSC03320_opt'
+  | '2023-llista-frontal/15_DSC03079'
+  | '2023-llista-frontal/16_DSC03215'
+  | '2023-llista-frontal/17_DSC03182'
