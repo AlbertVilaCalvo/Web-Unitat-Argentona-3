7489c3d63c4e577a97f1c2615787a3dbc5ab8d86
Albert Vila Calvo <4823860+AlbertVilaCalvo@users.noreply.github.com>
2023-04-24 21:16:15 +0200

    CloudinaryImage component

--- src/misc/cloudinary.ts
+++ src/misc/cloudinary.ts
@@ -1,3 +1,5 @@
 import { Cloudinary } from '@cloudinary/url-gen'
 
 export const cloudinary = new Cloudinary({ cloud: { cloudName: 'albertvila' } })
+
+export type CloudinaryImageId = 'DSC02847c_copia'
