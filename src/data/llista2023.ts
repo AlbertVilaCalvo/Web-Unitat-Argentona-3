980caa96f167740b56752fc37146e690041439ae
Albert Vila Calvo <4823860+AlbertVilaCalvo@users.noreply.github.com>
2023-05-18 18:28:36 +0200

    Home - Llista cercle images

--- src/data/llista2023.ts
+++ src/data/llista2023.ts
@@ -2,77 +2,95 @@ import type { CloudinaryImageId } from '../misc/cloudinary'
 
 interface Candidat {
   nom: string
-  image: CloudinaryImageId
+  imageFrontal: CloudinaryImageId
+  imageCercle: CloudinaryImageId
 }
 
 export const llista2023: Candidat[] = [
   {
     nom: 'Toni Segarra',
-    image: '2023-llista-frontal/1_DSC02904',
+    imageFrontal: '2023-llista-frontal/1_DSC02904',
+    imageCercle: '2023-llista-cercle/1_DSC02904-min',
   },
   {
     nom: 'Isabel Sunyer',
-    image: '2023-llista-frontal/2_DSC02769b1',
+    imageFrontal: '2023-llista-frontal/2_DSC02769b1',
+    imageCercle: '2023-llista-cercle/2_DSC02769b1-min',
   },
   {
     nom: 'Joan Boba',
-    image: '2023-llista-frontal/3_DSC02803b_opt',
+    imageFrontal: '2023-llista-frontal/3_DSC02803b_opt',
+    imageCercle: '2023-llista-cercle/3_DSC02803b-min',
   },
   {
     nom: 'Isabel Lladó',
-    image: '2023-llista-frontal/4_DSC02941b_opt',
+    imageFrontal: '2023-llista-frontal/4_DSC02941b_opt',
+    imageCercle: '2023-llista-cercle/4_DSC02941b-min',
   },
   {
     nom: 'Isidre Viñas',
-    image: '2023-llista-frontal/5_DSC02818b_opt',
+    imageFrontal: '2023-llista-frontal/5_DSC02818b_opt',
+    imageCercle: '2023-llista-cercle/5_DSC02818b-min',
   },
   {
     nom: 'Rut Ligos',
-    image: '2023-llista-frontal/6_DSC03096',
+    imageFrontal: '2023-llista-frontal/6_DSC03096',
+    imageCercle: '2023-llista-cercle/6_DSC03096-min',
   },
   {
     nom: 'Eusebi Traby',
-    image: '2023-llista-frontal/7_DSC02755_opt',
+    imageFrontal: '2023-llista-frontal/7_DSC02755_opt',
+    imageCercle: '2023-llista-cercle/7_DSC02755-min',
   },
   {
     nom: 'Araceli Ferrer',
-    image: '2023-llista-frontal/8_DSC02819',
+    imageFrontal: '2023-llista-frontal/8_DSC02819',
+    imageCercle: '2023-llista-cercle/8_DSC02819-min',
   },
   {
     nom: 'Xavier Rodríguez',
-    image: '2023-llista-frontal/9_DSC03094',
+    imageFrontal: '2023-llista-frontal/9_DSC03094',
+    imageCercle: '2023-llista-cercle/9_DSC03094-min',
   },
   {
     nom: 'Imma Collet',
-    image: '2023-llista-frontal/10_DSC03089',
+    imageFrontal: '2023-llista-frontal/10_DSC03089',
+    imageCercle: '2023-llista-cercle/10_DSC03089-min',
   },
   {
     nom: 'Joan Vera',
-    image: '2023-llista-frontal/11_DSC02897',
+    imageFrontal: '2023-llista-frontal/11_DSC02897',
+    imageCercle: '2023-llista-cercle/11_DSC02897-min',
   },
   {
     nom: 'Elizabeth Guirado',
-    image: '2023-llista-frontal/12_DSC03195',
+    imageFrontal: '2023-llista-frontal/12_DSC03195',
+    imageCercle: '2023-llista-cercle/12_DSC03195-min',
   },
   {
     nom: 'Rosend',
-    image: '2023-llista-frontal/13_DSC03178',
+    imageFrontal: '2023-llista-frontal/13_DSC03178',
+    imageCercle: '2023-llista-cercle/13_DSC03178-min',
   },
   {
     nom: 'Monste Terricabras',
-    image: '2023-llista-frontal/14_DSC03320_opt',
+    imageFrontal: '2023-llista-frontal/14_DSC03320_opt',
+    imageCercle: '2023-llista-cercle/14_DSC03320-min',
   },
   {
     nom: 'Albert Vila',
-    image: '2023-llista-frontal/15_DSC03079',
+    imageFrontal: '2023-llista-frontal/15_DSC03079',
+    imageCercle: '2023-llista-cercle/15_DSC03082',
   },
   {
     nom: 'Isabel Cunyat',
-    image: '2023-llista-frontal/16_DSC03215',
+    imageFrontal: '2023-llista-frontal/16_DSC03215',
+    imageCercle: '2023-llista-cercle/16_DSC03215-min',
   },
   {
     nom: 'Savino Rico',
-    image: '2023-llista-frontal/17_DSC03182',
+    imageFrontal: '2023-llista-frontal/17_DSC03182',
+    imageCercle: '2023-llista-cercle/17_DSC03182-min',
   },
   /*
   {
