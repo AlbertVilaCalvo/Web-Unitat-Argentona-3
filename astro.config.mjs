8e1d6499206d675baafc2f88ab9b9d2b66ceca4a
Albert Vila Calvo <4823860+AlbertVilaCalvo@users.noreply.github.com>
2025-01-07 13:46:44 +0100

    Upgrade to Astro 5 + upgrade other packages
    npx @astrojs/upgrade@latest
    npm i -D -E prettier@latest prettier-plugin-astro@latest typescript@latest
    
    See upgrade guide: https://docs.astro.build/en/guides/upgrade-to/v5/
    
    I've modifed tsconfig.json and delete src/env.d.ts, as explained in
    https://docs.astro.build/en/guides/upgrade-to/v5/#changed-typescript-configuration
    
    > Note that src/env.d.ts is only necessary if you have added custom configurations,
    > or if you’re not using a tsconfig.json file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,4 +12,7 @@ export default defineConfig({
     //  https://github.com/withastro/roadmap/blob/main/proposals/0036-inline-stylesheets.md
     //  inlineStylesheets: "auto",
   },
+  legacy: {
+    collections: true,
+  },
 })
